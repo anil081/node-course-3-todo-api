722deb5934d6e18d8710b50b4ffda1947d0a59b5
anil081 <anil.yadav081@gmail.com>
2018-07-28 00:00:07 +0530

    Add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app}  = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: 'Fist test todo'
+}, {
+	text: 'Second test todo'
+}];
+
+
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 
@@ -25,7 +35,7 @@ describe('POST/todos', () => {
 		    		return done(err);
 		    	}
 
-		    	Todo.find().then((todos) => {
+		    	Todo.find({text}).then((todos) => {
 		    		expect(todos.length).toBe(1);
 		    		expect(todos[0].text).toBe(text);
 		    		done();
@@ -45,10 +55,23 @@ it('should not create todo with invalid data', (done) => {
      		return done(err);
      	}
      	Todo.find().then((todos) => {
-     		expect(todos.length).toBe(0);
+     		expect(todos.length).toBe(2);
      		done();
      	}).catch((e) => done(e));
      });
 });
 
+});
+
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+      request(app)
+         .get('/todos')
+         .expect(200)
+         .expect((res) => {
+         	expect(res.body.todos.length).toBe(2);
+         })
+         .end(done);
+	});
 });
